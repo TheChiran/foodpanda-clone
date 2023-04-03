765627bc55bfecbb2deeb3b0bf47bcc8fd664a49
TheChiran <tonmoychiran449@gmail.com>
2023-04-03 21:33:56 +0600

    [Task-> 1. Included react-leaflet map 2. Included location set of users 3. Added change marker position on drag]

--- src/common/components/MapLocationSelectorModal/Index.tsx
+++ src/common/components/MapLocationSelectorModal/Index.tsx
@@ -1,44 +1,88 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import "./styles.scss";
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+// import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
+import { LatLngExpression, LatLngTuple } from 'leaflet';
 
 interface IProps {
     isOpen: boolean;
     onToggle: MouseEventHandler;
 };
 
+function LocationMarker({ markerPosition }: any) {
+    const [locationMarkerposition, setLocationMarkerPosition] = useState<LatLngExpression | null>(null);
+    const updateMarkerPosition = (event: any) => {
+        const latlng = event.target.getCenter();
+        const lat = latlng.lat;
+        const lng = latlng.lng;
+
+        setLocationMarkerPosition({ lat, lng });
+    };
+
+    const map = useMapEvents({
+        drag(event: any) {
+            updateMarkerPosition(event);
+        },
+        moveend(event: any) {
+            updateMarkerPosition(event);
+        }
+    });
+
+    if (locationMarkerposition === null && markerPosition === null) return null;
+    if (locationMarkerposition !== null) return (<Marker position={locationMarkerposition} draggable>
+        <Popup>You are here</Popup>
+    </Marker>);
+
+    return (<Marker position={{ lat: markerPosition[0], lng: markerPosition[1] }} draggable>
+        <Popup>You are here</Popup>
+    </Marker>);
+};
+
 export default function MapLocationSelectorModal({ isOpen, onToggle }: IProps) {
+    const [markerPosition, setMarkerPosition] = useState<LatLngExpression | null>(null);
+
+    useEffect(() => {
+        const getUserLocation = () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position: any) => {
+                    const { latitude, longitude } = position.coords;
+                    setMarkerPosition([latitude, longitude]);
+                }, (error) => {
+                    setMarkerPosition([23.8103, 90.4125]);
+                });
+            } else {
+                alert("Sorry! Your browser dosen't support geolocation API!");
+            }
+        };
+
+        getUserLocation();
+    }, []);
+
     return (
         <Dialog.Root defaultOpen={false} open={isOpen} modal={true}>
             <Dialog.Portal>
                 <Dialog.Overlay className="DialogOverlay" onClick={onToggle} />
                 <Dialog.Content className="DialogContent">
-                    <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-                    <Dialog.Description className="DialogDescription">
-                        Make changes to your profile here. Click save when you're done.
-                    </Dialog.Description>
-                    <fieldset className="Fieldset">
-                        <label className="Label" htmlFor="name">
-                            Name
-                        </label>
-                        <input className="Input" id="name" defaultValue="Pedro Duarte" />
-                    </fieldset>
-                    <fieldset className="Fieldset">
-                        <label className="Label" htmlFor="username">
-                            Username
-                        </label>
-                        <input className="Input" id="username" defaultValue="@peduarte" />
-                    </fieldset>
-                    <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
-                        <Dialog.Close asChild>
-                            <button className="Button green">Save changes</button>
-                        </Dialog.Close>
+                    <div className="title-section">
+                        <h2 className="title">Is this your exact location?</h2>
+                        <div className="close-btn" onClick={onToggle}>
+                            <div className="material-icons">clear</div>
+                        </div>
                     </div>
-                    <Dialog.Close asChild>
-                        <button className="IconButton" aria-label="Close" onClick={onToggle}>
-                            Close
-                        </button>
-                    </Dialog.Close>
+                    <TextField id="outlined-basic" label="Enter Full Address" variant="outlined"
+                        fullWidth
+                    />
+                    <div className="location-map" style={{ width: '100%', height: '250px' }}>
+                        {markerPosition !== null ? <MapContainer center={markerPosition as LatLngExpression} zoom={13} scrollWheelZoom={true} style={{ width: '100%', height: '100%' }}>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <LocationMarker markerPosition={markerPosition} />
+                        </MapContainer> : null}
+                    </div>
                 </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App'
 import { RouterProvider } from "react-router-dom";
 import { router } from './routes/router';
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
