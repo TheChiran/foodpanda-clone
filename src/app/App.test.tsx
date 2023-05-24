ee57518df3f57c1367882491d3ccb798252afd77
chiranSWE <tonmoychiran449@gmail.com>
2023-05-24 19:43:33 +0600

    [Task-> 1. Integrated jest test ecosystem 2. Wrote unit test for city component]

---
+++ src/app/App.test.tsx
@@ -0,0 +1,13 @@
+import { render,screen } from "@testing-library/react";
+
+import App from "./App";
+
+jest.mock('react-leaflet', () => {
+  return {}
+});
+
+test("Renders main page correctly", () => {
+  render(<App />);
+  
+  expect(true).toBeTruthy();
+});
